ab3a04ae46343653ba9b7d6ba000f124ad0326bc
Lazaro Manuel <lmonzongonzalez2019@gmail.com>
2023-10-26 03:21:27 +0200

    Primer Commit Estableciendo Conexion en la Clase Conector

---
+++ src/main/java/org/example/platzi/main/Main.java
@@ -0,0 +1,12 @@
+package org.example.platzi.main;
+
+import org.example.platzi.main.module.Conector;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Conector conectar = new Conector("jdbc:mysql://localhost:3306/platziblog","root","Lazaro12345@*");
+
+    }
+}
