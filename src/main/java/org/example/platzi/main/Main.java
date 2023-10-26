1a290d23a9fbc39d5dd1296583a1baca8b0ac5f9
Lazaro Manuel <lmonzongonzalez2019@gmail.com>
2023-10-26 03:38:39 +0200

    Optimizando Recursos

--- src/main/java/org/example/platzi/main/Main.java
+++ src/main/java/org/example/platzi/main/Main.java
@@ -1,12 +1,27 @@
 package org.example.platzi.main;
 
-import org.example.platzi.main.module.Conector;
+import java.sql.*;
 
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
-        Conector conectar = new Conector("jdbc:mysql://localhost:3306/platziblog","root","Lazaro12345@*");
+        String url = "jdbc:mysql://localhost:3306/platziblog";
+        String user = "root";
+        String password = "Lazaro12345@*";
 
+        try(Connection myConn = DriverManager.getConnection(url,user,password);
+            Statement myStamt = myConn.createStatement();
+            ResultSet resultSet = myStamt.executeQuery("SELECT * FROM people");){
+
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("frist_name"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Algo Salio Mal");
+        }
+        
     }
 }
